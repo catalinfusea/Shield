ef9cbf8950f1f5384974de33eb48af75b4b489fd
BenyH <Beny.Haddad@ericom.com>
2017-01-18 16:56:08 +0200

    Update and rename run to run.sh

---
+++ Dev15/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Ericom Shield
+# Launch Dockers for Ericom Shield
+
+echo "***********       Launching docker-compose up"
+echo "                  consul=3"
+echo "                  consului=1 "
+echo "                  elk=1 "
+echo "                  shield-broker=1 "
+echo "                  shield-browser=1 "
+echo "                  shield-proxy=1 "
+echo "                  icap-server=1"
+echo "***********       "
+docker-compose up -d && docker-compose scale consul=3 consului=1 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
+# && docker-compose logs
