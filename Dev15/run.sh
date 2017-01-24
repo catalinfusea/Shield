ed5d5e425f339e8cd018382f35b0d1bd4902ec27
ErezPasternak <erez.pasternak@gmail.com>
2017-01-24 14:01:53 +0200

    updated with new names and proxy

--- Dev15/run.sh
+++ Dev15/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-############################################
-#####   Ericom Shield Run.sh              ##
-#####   Launch Dockers for Ericom Shield  ##
-#######################################BH###
-
-
+# Ericom Shield
+# Launch Dockers for Ericom Shield
 
 echo "***********       Launching docker-compose up"
 echo "                  consul=3"
@@ -15,5 +11,5 @@ echo "                  shield-browser=1 "
 echo "                  shield-proxy=1 "
 echo "                  icap-server=1"
 echo "***********       "
-docker-compose up -d && docker-compose scale consul=3 consului=1 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
+docker-compose up -d && docker-compose scale consul=3 tech-admin=1 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
 # && docker-compose logs
