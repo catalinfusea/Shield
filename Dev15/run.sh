b3f947814c14e1144aadc2aecd9641f81591e7bf
ErezPasternak <erez.pasternak@gmail.com>
2017-01-24 16:43:26 +0200

    removed tech admin for now

--- Dev15/run.sh
+++ Dev15/run.sh
@@ -11,5 +11,5 @@ echo "                  shield-browser=1 "
 echo "                  shield-proxy=1 "
 echo "                  icap-server=1"
 echo "***********       "
-docker-compose up -d && docker-compose scale consul=3 tech-admin=1 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
+docker-compose up -d && docker-compose scale consul=3 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
 # && docker-compose logs
