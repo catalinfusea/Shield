696063939279a8807d4ca9847473b4f7d0308217
ErezPasternak <erez.pasternak@gmail.com>
2017-01-26 11:29:57 +0200

    updated yaml, now we wait for ELK

--- Dev15/run.sh
+++ Dev15/run.sh
@@ -8,8 +8,8 @@ echo "                  consului=1 "
 echo "                  elk=1 "
 echo "                  shield-broker=1 "
 echo "                  shield-browser=1 "
-echo "                  shield-proxy=1 "
+echo "                  proxy-server=1 "
 echo "                  icap-server=1"
 echo "***********       "
-docker-compose up -d && docker-compose scale consul=3 elk=1 shield-broker=1 shield-browser=1 shield-proxy=1 icap-server=1
+docker-compose up -d && docker-compose scale consul=3 shield-admin=1 elk=1 shield-broker=1 shield-browser=1 proxy-server=1 icap-server=1
 # && docker-compose logs
