8b0045f2834f63987e70bbc38095d36871fc7f37
ErezPasternak <Erez.Pasternak@gmail.com>
2017-01-29 18:55:49 +0200

    Update ES-setup.sh

--- Dev15/ES-setup.sh
+++ Dev15/ES-setup.sh
@@ -45,8 +45,8 @@ service docker start
  
 #if [ $( which docker-compose | wc -l ) -eq 0 ]; then 
    echo "***************     Installing docker-compose"
-   curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-   chmod +x /usr/bin/docker-compose
+   curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+   chmod +x /usr/local/bin/docker-compose
 #else 
 #   echo "***************     DockerCompose is already installed"
 #fi
