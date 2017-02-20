fec7369dd253a420929921f55cd86813968b7710
ErezPasternak <erez.pasternak@gmail.com>
2017-02-20 12:01:52 +0200

    updated Dev16, now admin works

--- Dev-Feb16/Readme.txt
+++ Dev-Feb16/Readme.txt
@@ -7,7 +7,7 @@ Open a Shell:
 Create a new Folder and go to this folder
 
 Type the following commands:
-wget "https://raw.githubusercontent.com/ErezPasternak/Shield/master/Dev15/ES-setup.sh"
+wget "https://raw.githubusercontent.com/ErezPasternak/Shield/master/Dev-Feb16/ES-setup.sh"
 (optionally: dos2unix ES-setup.sh)
 chmod +x ES-setup.sh
 ./ES-setup.sh
---
+++ Dev-Feb16/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Ericom Shield
+# Launch Dockers for Ericom Shield
+
+echo "***********       Launching docker-compose up"
+echo "                  consul=3"
+echo "                  shield-admin=1 "
+echo "                  elk=1 "
+echo "                  shield-browser=1 "
+echo "                  proxy-server=1 "
+echo "                  icap-server=1"
+echo "***********       "
+docker-compose up -d && docker-compose scale consul=3 shield-admin=1 elk=1 shield-browser=1 proxy-server=1 icap-server=1
+# && docker-compose logs
