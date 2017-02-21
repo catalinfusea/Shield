9c90ff93ce1e4bcce3f6ea52406f1769e7352f34
BenyH <Beny.Haddad@ericom.com>
2017-02-21 15:00:14 +0200

    Update ES-setup.sh

--- Dev15/ES-setup.sh
+++ Dev15/ES-setup.sh
@@ -2,10 +2,12 @@
 ############################################
 #####   Ericom Shield Installer        #####
 #######################################BH###
-
 #Check if we are root
 if (( $EUID != 0 )); then
-    sudo su
+#    sudo su
+        echo " Please run it as Root"
+        echo "sudo" $0 $1 $2
+        exit
 fi
 
 # Development Repository: (Latest)
