b814ffc9526f37e3df15a20f6f0c9e66924860cd
ErezPasternak <erez.pasternak@gmail.com>
2017-03-02 10:17:11 +0200

    clean update script

--- Dev-Feb16/ES-update.sh
+++ Dev-Feb16/ES-update.sh
@@ -18,29 +18,6 @@ echo "***************     Installing Ericom Shield ..."
 curl -s -S -o docker-compose.yml $ES_repo_yml
 curl -s -S -o run.sh $ES_repo_run
 chmod +x run.sh
-
-if [ "$#" -eq 2 ]; then
-    #Login and enter the credentials you received separately when prompt
-    echo "docker login" $1 $2
-    docker login --username=$1 --password=$2
-else
-    echo " Please enter your login credentials to docker-hub"
-    docker login 
-fi
-    
-    if [ $? == 0 ]; then
-      echo "Login Succeeded!"
-    else
-      echo "Please try again:"
-      docker login 
-      if [ $? == 0 ]; then
-         echo "Login Succeeded!"
-        else
-         echo "Cannot Login, Exiting!"
-         exit
-      fi
-    fi
-
 ./run.sh
 
 echo "***************     Success!"
