a5d02a8419ff70400475079cadf33dfd3cef0a4c
BenyH <Beny.Haddad@ericom.com>
2017-03-02 13:54:02 +0200

    Create autoupdate.sh
    run it with sudo and &
    sudo ./autoupdate.sh > /dev/null &

---
+++ Dev-Feb16/autoupdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield AutoUpdate       #####
+#######################################BH###
+
+while true
+do
+        echo "."
+        ./ES-setup.sh
+        sleep 5m
+done
