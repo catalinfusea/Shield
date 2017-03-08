4d7bd2f8b7b6cb99c9f7b9da6ed0b759ef45b473
BenyH <Beny.Haddad@ericom.com>
2017-03-08 10:21:20 +0200

    Update autoupdate.sh

--- Dev-Feb16/autoupdate.sh
+++ Dev-Feb16/autoupdate.sh
@@ -6,9 +6,17 @@
 #  run this script in the background
 #  sudo nohup ./autoupdate.sh > /dev/null &
 
+ES_PATH="/usr/local/ericomshield"
+cd $ES_PATH
+
 while true
 do
         echo "."
         ./ES-setup.sh
         sleep 5m
+        ./status.sh
+        if [ $? -ne 0 ]; then
+          echo "ericomshield was not running"
+          ./run.sh
+        fi
 done
