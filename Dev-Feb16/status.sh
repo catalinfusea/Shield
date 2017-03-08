d3423865ed5096a97334574cfafbe1173cf8a1e9
BenyH <Beny.Haddad@ericom.com>
2017-03-08 10:15:23 +0200

    Create status.sh
    status script for the service

---
+++ Dev-Feb16/status.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield Run              #####
+#######################################BH###
+ES_PATH="/usr/local/ericomshield"
+ES_RUN="$ES_PATH/run.sh"
+
+#Check if we are root
+if (( $EUID != 0 )); then
+#    sudo su
+        echo " Please run it as Root"
+        echo "sudo" $0 $1 $2
+        exit
+fi
+
+NUM=`grep scale $ES_RUN | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /'+'/g'`
+DOCKERS_FOR_SHIELD=$(($NUM))
+echo $DOCKERS_FOR_SHIELD
+SHIELD_STRING=securebrowsing
+docker ps | grep $SHIELD_STRING |wc -l
+
+if [ $( docker ps | grep $SHIELD_STRING |wc -l ) -ge  $DOCKERS_FOR_SHIELD ]; then
+    echo "***************     Ericom Shield Dockers are running"
+  else
+   echo " Ericom Shield is not running properly on this system"
+   exit 1
+fi
+
+exit 0
