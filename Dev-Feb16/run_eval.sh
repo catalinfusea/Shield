8053237cff02e9a960be9f0bec188e8693b522e3
BenyH <Beny.Haddad@ericom.com>
2017-03-08 17:35:11 +0200

    Create run_eval.sh

---
+++ Dev-Feb16/run_eval.sh
@@ -0,0 +1,17 @@
+#####   Ericom Shield Run              #####
+#######################################BH###
+ES_PATH=/usr/local/ericomshield
+
+cd $ES_PATH
+
+echo "***********       Launching docker-compose up"
+echo "                  consul=3"
+echo "                  shield-admin=1 "
+echo "                  elk=1 "
+echo "                  shield-browser=5 "
+echo "                  proxy-server=1 "
+echo "                  icap-server=1"
+echo "***********       "
+docker-compose up -d && docker-compose scale consul=3 shield-admin=1 elk=1 shield-browser=5 proxy-server=1 icap-server=1
+
+# && docker-compose logs
