ac3903a721209a4172911054c5f236db9772ed5b
BenyH <Beny.Haddad@ericom.com>
2017-03-20 15:54:52 +0200

    Update ericomshield-setup.sh

--- Dev-Feb16/ericomshield-setup.sh
+++ Dev-Feb16/ericomshield-setup.sh
@@ -97,8 +97,10 @@ if [ $UPDATE -eq 0 ]; then
          apt-get update
          apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
          apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-         apt-get update
+         apt-get -y install apt-transport-https
          apt-cache policy docker-engine
+         apt-get --assume-yes install linux-image-extra-$(uname -r) linux-image-extra-virtual                  
+         apt-get update
          apt-get --assume-yes -y install docker-engine
 
     else
