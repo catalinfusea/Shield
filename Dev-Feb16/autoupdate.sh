737f5868a74b96d6defac1ac8d7ed692cb96f192
BenyH <Beny.Haddad@ericom.com>
2017-03-22 10:35:18 +0200

    Update autoupdate.sh

--- Dev-Feb16/autoupdate.sh
+++ Dev-Feb16/autoupdate.sh
@@ -5,14 +5,17 @@
 
 #  If you are not using ericomshield service, run this script in the background
 #  sudo nohup ./autoupdate.sh > /dev/null &
-
-ES_AUTO_UPDATE_FILE="$ES_PATH/.autoupdate"
 ES_PATH="/usr/local/ericomshield"
+ES_AUTO_UPDATE_FILE="$ES_PATH/.autoupdate"
+ES_repo_setup="https://raw.githubusercontent.com/ErezPasternak/Shield/master/Dev-Feb16/ericomshield-setup.sh"
+
 cd $ES_PATH
 
 while true
 do
         if [ -f "$ES_AUTO_UPDATE_FILE" ]; then
+           curl -s -S -o ericomshield-setup.sh $ES_repo_setup
+           chmod +x ericomshield-setup.sh
            ./ericomshield-setup.sh
         fi
         echo "."
