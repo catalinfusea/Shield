cdb4a7203afa4d04f069669bb7c75c6176ca4bd7
Beny H <Beny.Haddad@ericom.com>
2017-05-28 15:28:33 +0300

    Update stop.sh

--- Dev-Feb16/stop.sh
+++ Dev-Feb16/stop.sh
@@ -7,7 +7,7 @@ ES_PATH=/usr/local/ericomshield
 ES_SWARM_FILE="$ES_PATH/.esswarm"
 
 if [ -f "$ES_SWARM_FILE" ]; then
-   echo run.sh is not working for swarm mode 
+   echo stop.sh is not working for swarm mode 
  else
    cd $ES_PATH
    echo "***********       Stopping ericomshield dockers "
