93509eb222cd8da78836780d66c69b7c789a1d5d
Beny H <Beny.Haddad@ericom.com>
2017-05-28 15:28:05 +0300

    Update stop.sh

--- Dev-Feb16/stop.sh
+++ Dev-Feb16/stop.sh
@@ -2,9 +2,15 @@
 ############################################
 #####   Ericom Shield Stop             #####
 #######################################BH###
-ES_PATH=/usr/local/ericomshield
 
-cd $ES_PATH
-echo "***********       Stopping ericomshield dockers "
-echo "***********       "
-docker-compose down
+ES_PATH=/usr/local/ericomshield
+ES_SWARM_FILE="$ES_PATH/.esswarm"
+
+if [ -f "$ES_SWARM_FILE" ]; then
+   echo run.sh is not working for swarm mode 
+ else
+   cd $ES_PATH
+   echo "***********       Stopping ericomshield dockers "
+   echo "***********       "
+   docker-compose down
+ fi
