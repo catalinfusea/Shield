b33d25a709bea05a9fbdb7f08125b102e26578fc
Beny H <Beny.Haddad@ericom.com>
2017-06-08 11:48:41 +0300

    stop for swarm

--- Dev-Feb16/stop.sh
+++ Dev-Feb16/stop.sh
@@ -1,26 +1,20 @@
-#!/bin/bash +x
+#!/bin/bash
 ############################################
-#####   Ericom Shield Run              #####
+#####   Ericom Shield Stop             #####
 #######################################BH###
 
 ES_PATH=/usr/local/ericomshield
 ES_SWARM_FILE="$ES_PATH/.esswarm"
-STACK_NAME='shield'
-ES_YML_FILE=docker-compose.yml
+STACK_NAME=shield
 
 if [ -f "$ES_SWARM_FILE" ]; then
-   echo "swarm mode:"
-   docker stack deploy -c $ES_YML_FILE $STACK_NAME
- else
-   #cd $ES_PATH
-   echo "***********       Launching docker-compose up"
-   echo "                  consul=3"
-   echo "                  shield-admin=1 "
-   echo "                  elk=1 "
-   echo "                  shield-browser=20 "
-   echo "                  proxy-server=1 "
-   echo "                  icap-server=1"
+   echo "***********       Stopping EricomShield (swarm) "
    echo "***********       "
-   docker-compose -f docker-compose.yml up -d && docker-compose -f docker-compose.yml scale consul=3 shield-admin=1 elk=1 shield-browser=20 proxy-server=1 icap-server=1
-   # && docker-compose logs
-fi
+   docker stack rm $STACK_NAME
+#   docker swarm leave -f
+ else
+   cd $ES_PATH
+   echo "***********       Stopping EricomShield "
+   echo "***********       "
+   docker-compose down
+ fi
